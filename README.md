e4d6153a882df33791d8417a5bd127b05eb9bd97
Kevin Park <krapi0314@gmail.com>
2024-01-29 17:30:51 +0900

    Scaffold project

---
+++ README.md
@@ -0,0 +1,42 @@
+# plumber
+
+Plumber is a L7 load balancer from scratch in Go
+
+## Installation
+
+```bash
+# Clone the repository
+git clone https://github.com/krapie/plumber.git
+
+# Build the binary
+cd plumber
+make build
+```
+
+## Usage
+
+```bash
+# Start the load balancer with 2 backends
+./bin/plumber --backends http://localhost:8080,http://localhost:8081
+```
+
+## Roadmap
+
+Plumber aims to support [Yorkie](https://github.com/yorkie-team/yorkie) as a backend for the load balancer.
+The following features are planned to be implemented first:
+
+### v0.0.1
+
+- [ ] Support static load balancing with round-robin algorithm
+- [ ] Support backends health check 
+
+### v0.1.0
+
+- [ ] Support consistent hashing algorithm
+- [ ] Support dynamic backend configuration (with K8s API)
+- [ ] Support mechanism to resolve split-brain of long-lived connection
+
+### v0.2.0
+
+- [ ] Support interceptor to modify request/response
+- [ ] TBD
---
+++ cmd/root.go
@@ -0,0 +1,86 @@
+/*
+Copyright 2024 Kevin Park
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "plumber",
+	Short: "Plumber is a L7 load balancer from scratch in Go.",
+	Long:  `Plumber is a L7 load balancer from scratch in Go.`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Plumber is a L7 load balancer from scratch in Go.")
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.plumber.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+
+		// Search config in home directory with name ".plumber" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".plumber")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
